3893dae2265763e383aa77f199e7cab3a8003eb0
Nick Liu <theaznnoobie@gmail.com>
2016-10-23 15:40:58 -0400

    Subsituted Symbols

--- assemble.py
+++ assemble.py
@@ -12,7 +12,6 @@ class Assembler():
         # 2. make symbol tables
         # 3. substitute symbol table
         # 4. translate to bytecode
-
         contents = self.read_and_clean_asm(filename)
         (sym_table, contents) = self.build_sym_table(contents)
         contents = self.split_ops(contents)
@@ -62,11 +61,23 @@ class Assembler():
 
     def substitute_symbols(self, contents, sym_table):
         for (pc, type, data) in contents:
+            data = list(data)
+            data = [str(sym_table[n]) if n in sym_table.keys() else n for n in data]
+            data = [str(self.reg_table[n]) if n in self.reg_table.keys() else n for n in data]
+            if "(" and ")" in data[-1]:
+                data[-1] = [", ".join(x.split()) for x in re.split(r'[()]',data[-1]) if x.strip()]
+                data[-1] = [str(sym_table[n]) if n in sym_table.keys() else n for n in data[-1]]
+                data[-1] = [str(self.reg_table[n]) if n in self.reg_table.keys() else n for n in data[-1]]
+                data[-1] = str(int(data[-1][0]) + int(data[-1][1]))
+            if "x" in data[-1]:
+                data[-1] = int(data[-1], 16)
             yield (pc, type, data)
 
     def translate_asm(self, sub_contents):
-        return []
+        for (pc, type, data) in contents:
+            op = data[1]
 
+            yield (pc, type, data)
     ###
     # R0..R3 are also A0..A3 (function arguments, caller saved)
     # R3 is also RV (return value, caller saved)
@@ -81,19 +92,16 @@ class Assembler():
     ###
     def build_reg_table(self):
         regs = {"R{0}".format(i): i for i in range(0, 16)}
-        regs.update({"A{0}".format(i): regs["R{0}".format(i)] for i in range(0, 3)})
+        regs.update({"A{0}".format(i): regs["R{0}".format(i)] for i in range(0, 4)})
         regs.update({"RV": regs["R3"]})
-        regs.update({"T{0}".format(i): regs["R{0}".format(i + 4)] for i in range(0, 1)})
-        regs.update({"S{0}".format(i): regs["R{0}".format(i + 6)] for i in range(0, 2)})
+        regs.update({"T{0}".format(i): regs["R{0}".format(i + 4)] for i in range(0, 2)})
+        regs.update({"S{0}".format(i): regs["R{0}".format(i + 6)] for i in range(0, 3)})
         regs.update({"GP": regs["R12"]})
         regs.update({"FP": regs["R13"]})
         regs.update({"SP": regs["R14"]})
         regs.update({"RA": regs["R15"]})
+        regs.update({key.lower(): regs[key] for key in regs.keys()})
         return regs
-
-    def build_op_table(self):
-        # todo fill table
-        return {"ADD": 0x00000000}
 
     def parse_literal(self, num):
         return int(num, 0)
@@ -122,6 +130,60 @@ class Assembler():
 
         return "OP", [l]
 
+    def build_op_table(self):
+        opcodes = [
+            {'instr': 'ADD', 'type': 'ALU-R', 'opcode' : "11000111" },
+            {'instr': 'SUB', 'type': 'ALU-R', 'opcode' : "11000110" },
+            {'instr': 'AND', 'type': 'ALU-R', 'opcode' : "11000000" },
+            {'instr': 'OR', 'type': 'ALU-R', 'opcode' : "11000001" },
+            {'instr': 'XOR', 'type': 'ALU-R', 'opcode' : "11000010" },
+            {'instr': 'NAND', 'type': 'ALU-R', 'opcode' : "11001000" },
+            {'instr': 'NOR', 'type': 'ALU-R', 'opcode' : "11001001" },
+            {'instr': 'XNOR', 'type': 'ALU-R', 'opcode' : "11001010" },
+            {'instr': 'ADDI', 'type': 'ALU-I', 'opcode' : '01000111'},
+            {'instr': 'SUBI', 'type': 'ALU-I', 'opcode' : '01000110'},
+            {'instr': 'ANDI', 'type': 'ALU-I', 'opcode' : '01000000'},
+            {'instr': 'ORI', 'type': 'ALU-I', 'opcode' : '01000001'},
+            {'instr': 'XORI', 'type': 'ALU-I', 'opcode' : '01000010'},
+            {'instr': 'NANDI', 'type': 'ALU-I', 'opcode' : '01001000'},
+            {'instr': 'NORI', 'type': 'ALU-I', 'opcode' : '01001001'},
+            {'instr': 'XNORI', 'type': 'ALU-I', 'opcode' : '01001010'},
+            {'instr': 'MVHI', 'type': 'ALU-I', 'opcode' : '01001111'},
+            {'instr': 'LW ', 'type': 'LDSW', 'opcode': '01110000'},
+            {'instr': 'SW ', 'type': 'LDSW', 'opcode': '00110000'},
+            {'instr': 'F', 'type': 'CMP-R', 'opcode': '11010011'},
+            {'instr': 'EQ', 'type': 'CMP-R', 'opcode': '11010110'},
+            {'instr': 'LT', 'type': 'CMP-R', 'opcode': '11011001'},
+            {'instr': 'LTE', 'type': 'CMP-R', 'opcode': '11011100'},
+            {'instr': 'T', 'type': 'CMP-R', 'opcode': '11010000'},
+            {'instr': 'NE', 'type': 'CMP-R', 'opcode': '11010101'},
+            {'instr': 'GTE', 'type': 'CMP-R', 'opcode': '11011010'},
+            {'instr': 'GT', 'type': 'CMP-R', 'opcode': '11011111'},
+            {'instr': 'FI', 'type': 'CMP-I', 'opcode' : '01010011'},
+            {'instr': 'EQI', 'type': 'CMP-I', 'opcode' : '01010110'},
+            {'instr': 'LTI', 'type': 'CMP-I', 'opcode' : '01011001'},
+            {'instr': 'LTEI', 'type': 'CMP-I', 'opcode' : '01011100'},
+            {'instr': 'TI', 'type': 'CMP-I', 'opcode' : '01010000'},
+            {'instr': 'NEI', 'type': 'CMP-I', 'opcode' : '01010101'},
+            {'instr': 'GTEI', 'type': 'CMP-I', 'opcode' : '01011010'},
+            {'instr': 'GTI', 'type': 'CMP-I', 'opcode' : '01011111'},
+            {'instr': 'BF', 'type': 'BRANCH', 'opcode': '00100011'},
+            {'instr': 'BEQ', 'type': 'BRANCH', 'opcode': '00100110'},
+            {'instr': 'BLT', 'type': 'BRANCH', 'opcode': '00101001'},
+            {'instr': 'BLTE', 'type': 'BRANCH', 'opcode': '00101100'},
+            {'instr': 'BEQZ', 'type': 'BRANCH', 'opcode': '00100010'},
+            {'instr': 'BLTZ', 'type': 'BRANCH', 'opcode': '00101101'},
+            {'instr': 'BLTEZ', 'type': 'BRANCH', 'opcode': '00101000'},
+            {'instr': 'BT', 'type': 'BRANCH', 'opcode': '00100000'},
+            {'instr': 'BNE', 'type': 'BRANCH', 'opcode': '00100101'},
+            {'instr': 'BGTE', 'type': 'BRANCH', 'opcode': '00101010'},
+            {'instr': 'BGT', 'type': 'BRANCH', 'opcode': '00101011'},
+            {'instr': 'BNEZ', 'type': 'BRANCH', 'opcode': '00100001'},
+            {'instr': 'BGTEZ', 'type': 'BRANCH', 'opcode': '00101110'},
+            {'instr': 'BGTZ', 'type': 'BRANCH', 'opcode': '00101111'},
+            {'instr': 'JAL', 'type': 'BRANCH', 'opcode': '01100000'}
+        ]
+        return opcodes
 
 def main():
     parser = argparse.ArgumentParser()
